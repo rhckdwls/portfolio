683487addd246d7669be726f9e9e555a6c16b9d9
rhckdwls <92511809+rhckdwls@users.noreply.github.com>
2021-10-16 18:27:16 +0900

    JS_navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// 네브바 스크롤 내릴시 고정 효과 투명
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
