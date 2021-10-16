9a777e79474f7c053013deea1e283d39ae6a5696
rhckdwls <92511809+rhckdwls@users.noreply.github.com>
2021-10-16 22:37:35 +0900

    CSS_home button

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const navbarMenu = document.querySelector('.navbar__menu');
 
 navbarMenu.addEventListener('click', (event) => {
 
-  const target = event.target;
+const target = event.target;
 
   const link = target.dataset.link;
 
@@ -27,12 +27,20 @@ navbarMenu.addEventListener('click', (event) => {
 
   } 
 
-  console.log(event.target.dataset.link);
-
-  const scrollTo = document.querySelector(link);
-
-  scrollTo.scrollIntoView({behavior: "smooth" });
-
- 
+  scrollIntoView(link);
 
 });
+
+// 버튼 클릭시 이동 -콘텍트 미
+const homebtn = document.querySelector('.home__contact');
+
+homebtn.addEventListener('click', () => {
+
+    scrollIntoView('#contact');
+
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+}
